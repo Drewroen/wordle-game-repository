2b1a75cf45471e7ef22178e836e0979c93a5cd38
Drew Roen <drewroen@gmail.com>
2022-03-21 21:26:00 -0500

    Initial commit

---
+++ wordles.js
@@ -0,0 +1,252 @@
+var wordles = [
+  {
+    name: "Wordle",
+    game: "https://www.nytimes.com/games/wordle/index.html",
+    icon: "https://cdn.mobilesyrup.com/wp-content/uploads/2022/01/wordle.png",
+    description: "The OG wordle"
+  },
+  {
+    name: "Dordle",
+    game: "https://zaratustra.itch.io/dordle",
+    icon: "https://www2.minijuegosgratis.com/v3/games/thumbnails/240534_1.jpg",
+    description: "Wordle, but you do two at the same time"
+  },
+  {
+    name: "Quordle",
+    game: "https://www.quordle.com/#/",
+    icon: "https://progameguides.com/wp-content/uploads/2022/02/Featured-Quordle-Title-900x506.jpg",
+    description: "Two was too easy? Try four!"
+  },
+  {
+    name: "Octordle",
+    game: "https://octordle.com/",
+    icon: "https://world3dmap.com/wp-content/uploads/octordle.webp",
+    description: "Alright, you asked for it, eight words at once"
+  },
+  {
+    name: "Sedecordle",
+    game: "https://www.sedecordle.com/",
+    icon: "https://img.fresherslive.com/latestnews/images/articles/origin/2022/02/22/sedecordle-word-game-62148da87a0c0-1645514152.jpg",
+    description: "I think you get the point..."
+  },
+  {
+    name: "Worldle",
+    game: "https://worldle.teuteuf.fr/",
+    icon: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTOgicFa41LRH2met1Ggy1aaUWBysSaFS2iSA&usqp=CAU",
+    description: "Guess the correct country"
+  },
+  {
+    name: "Nerdle",
+    game: "https://nerdlegame.com/",
+    icon: "https://nerdlegame.com/logo192.png",
+    description: "Guess the correct math equation"
+  },
+  {
+    name: "Semantle",
+    game: "https://semantle.novalis.org/",
+    icon: "https://www.fluentu.com/blog/portuguese/wp-content/uploads/sites/29/2018/11/portuguese-language-exchange-6.png",
+    description: "Instead of spelling, you are guessing based off definition"
+  },
+  {
+    name: "Heardle",
+    game: "https://www.heardle.app/",
+    icon: "https://cdn.nerdschalk.com/wp-content/uploads/2022/03/heardle-logo-759x427.png?width=800",
+    description: "Guess the correct song"
+  },
+  {
+    name: "Globle",
+    game: "https://globle-game.com/",
+    icon: "https://images.dailyhive.com/20220213124629/world.jpg",
+    description: "Find the correct country based on geographical location"
+  },
+  {
+    name: "Hordle",
+    game: "https://hordle.legboot.com/",
+    icon: "https://icons.iconarchive.com/icons/google/noto-emoji-animals-nature/1024/22227-horse-icon.png",
+    description: "Guess different spellings of Horse. No, I'm not happy about it either..."
+  },
+  {
+    name: "AntiWordle",
+    game: "https://www.antiwordle.com/",
+    icon: "https://www.antiwordle.com/antiwordle.png",
+    description: "Avoid guessing the correct word"
+  },
+  {
+    name: "Airportle",
+    game: "https://airportle.scottscheapflights.com/",
+    icon: "https://airportle.scottscheapflights.com/opengraph.png",
+    description: "Guess the correct airport code"
+  },
+  {
+    name: "Squirdle",
+    game: "https://squirdle.fireblend.com/daily.html",
+    icon: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/007.png",
+    description: "Guess the correct pokemon"
+  },
+  {
+    name: "Waffle",
+    game: "https://wafflegame.net/",
+    icon: "https://world3dmap.com/wp-content/uploads/waffle-game.webp",
+    description: "Solve the crossword-like wordle"
+  },
+  {
+    name: "Taylordle",
+    game: "https://www.taylordle.com/",
+    icon: "https://www.rollingstone.com/wp-content/uploads/2022/01/GettyImages-1350322887c.jpg?resize=1800,1200&w=1800",
+    description: "Solve this Taylor Swift themed wordle"
+  },
+  {
+    name: "Gordle",
+    game: "https://gordle.herokuapp.com/",
+    icon: "https://walkthroughs.net/wp-content/uploads/2022/03/gordle-answer-for-today-scaled.jpeg",
+    description: "The answer is a hockey player's last name"
+  },
+  {
+    name: "CFBordle",
+    game: "https://cfbordle.redditcfb.com/",
+    icon: "https://external-preview.redd.it/o8NrtlKsouosmfHUCISgJopQBA73-AkT_mfoOjMXe3Q.jpg?auto=webp&s=5252f8c1dce70423731a0abf034866fbfa465532",
+    description: "Solve this football themed wordle"
+  },
+  {
+    name: "Lewdle",
+    game: "https://www.lewdlegame.com/",
+    icon: "https://cdn.mos.cms.futurecdn.net/YoKfgfdKQkpRFVADmJp9T7.jpg",
+    description: "NSFW words only, exercise caution!"
+  },
+  {
+    name: "Primel",
+    game: "https://converged.yt/primel/",
+    icon: "https://study.com/cimages/multimages/16/dkfjlajlk1736882588176226979.png",
+    description: "Guess the prime number"
+  },
+  {
+    name: "",
+    game: "",
+    icon: "",
+    description: ""
+  },
+  {
+    name: "",
+    game: "",
+    icon: "",
+    description: ""
+  },
+  {
+    name: "",
+    game: "",
+    icon: "",
+    description: ""
+  },
+  {
+    name: "",
+    game: "",
+    icon: "",
+    description: ""
+  },
+  {
+    name: "",
+    game: "",
+    icon: "",
+    description: ""
+  },
+  {
+    name: "",
+    game: "",
+    icon: "",
+    description: ""
+  },
+  {
+    name: "",
+    game: "",
+    icon: "",
+    description: ""
+  },
+  {
+    name: "",
+    game: "",
+    icon: "",
+    description: ""
+  },
+  {
+    name: "",
+    game: "",
+    icon: "",
+    description: ""
+  },
+  {
+    name: "",
+    game: "",
+    icon: "",
+    description: ""
+  },
+  {
+    name: "",
+    game: "",
+    icon: "",
+    description: ""
+  },
+  {
+    name: "",
+    game: "",
+    icon: "",
+    description: ""
+  },
+  {
+    name: "",
+    game: "",
+    icon: "",
+    description: ""
+  },
+  {
+    name: "",
+    game: "",
+    icon: "",
+    description: ""
+  },
+  {
+    name: "",
+    game: "",
+    icon: "",
+    description: ""
+  },
+  {
+    name: "",
+    game: "",
+    icon: "",
+    description: ""
+  },
+  {
+    name: "",
+    game: "",
+    icon: "",
+    description: ""
+  },
+  {
+    name: "",
+    game: "",
+    icon: "",
+    description: ""
+  }
+];
+
+for (var wordle of wordles)
+{
+  if (wordle.name != "")
+  document.getElementById("wordles").innerHTML += `
+  <div class="wordle">
+                  <img src="${wordle.icon}" alt="${wordle.name}">
+                  <a href="${wordle.game}" title="${wordle.name}">
+                      <div class="wordle-overlay">
+                          <div class="overlay-text">
+                              <div class="overlay-text-header">
+                              ${wordle.name}
+                              </div>
+                              <div class="overlay-text-details">
+                              ${wordle.description}
+                              </div>
+                          </div>
+                      </div>
+                  </a>
+              </div>
+  `;
+}
